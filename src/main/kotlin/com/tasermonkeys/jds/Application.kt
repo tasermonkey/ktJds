ee8a9905a20d62117bef3cdf761d54395d8ce579
James Stapleton <github@tasermonkeys.com>
2016-12-23 11:24:52 -0500

    --

--- src/main/kotlin/com/tasermonkeys/jds/Application.kt
+++ src/main/kotlin/com/tasermonkeys/jds/Application.kt
@@ -1,13 +1,23 @@
 package com.tasermonkeys.jds
 
 import com.fasterxml.jackson.module.kotlin.KotlinModule
+import org.elasticsearch.client.Client
 import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
 
+@EnableAutoConfiguration
+@Configuration
 @SpringBootApplication
 open class Application {
+    @Bean
+    open fun elasticsearchClient(): Client? {
+        return null;
+    }
+
     @Bean
     open fun objectMapperBuilder(): Jackson2ObjectMapperBuilder
             = Jackson2ObjectMapperBuilder().modulesToInstall(KotlinModule())
